2fe214aa68207813803b6ba1818c4b8755503f19
Ryan <balseiro1029@gmail.com>
2025-03-11 19:51:11 -0400

    create composable for list item cards

--- app/src/main/java/com/ryanbalseiro/countrieslist/ui/CountryCard.kt
+++ app/src/main/java/com/ryanbalseiro/countrieslist/ui/CountryCard.kt
@@ -3,24 +3,31 @@ package com.ryanbalseiro.countrieslist.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CountryCard(name: String, region: String, code: String, capital: String) {
-    Card() {
-        Column {
+    Card(modifier = Modifier.widthIn(60.dp, 300.dp).heightIn(60.dp, 60.dp)) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
             Row {
-                Text(name)
+                Text("$name, ")
                 Text(region)
                 Spacer(Modifier.width(6.dp))
                 Text(code)
             }
+            Spacer(Modifier.height(10.dp))
             Text(capital)
         }
     }
