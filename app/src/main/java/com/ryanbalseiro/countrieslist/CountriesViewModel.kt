513df31621b358273718fd755af157ce67207f72
Ryan <balseiro1029@gmail.com>
2026-03-31 15:22:54 -0400

    search by name implemented

--- app/src/main/java/com/ryanbalseiro/countrieslist/CountriesViewModel.kt
+++ app/src/main/java/com/ryanbalseiro/countrieslist/CountriesViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ryanbalseiro.countrieslist.data.api.CountriesIntent
 import com.ryanbalseiro.countrieslist.data.model.countries.Countries
+import com.ryanbalseiro.countrieslist.data.model.countries.CountriesItem
 import com.ryanbalseiro.countrieslist.data.repository.CountriesRepository
 import kotlinx.coroutines.launch
 
@@ -17,21 +18,42 @@ class CountriesViewModel: ViewModel() {
             CountriesRepository::class.java
             return CountriesRepository()
         }
-    private val _countriesList = MutableLiveData<Countries>()
-    val countriesList: LiveData<Countries> = _countriesList
+    private val _masterCountriesList = MutableLiveData<Countries>()
+    private val masterCountriesList: LiveData<Countries> = _masterCountriesList
+
+    private val _countriesList = MutableLiveData<List<CountriesItem>>()
+    val countriesList: LiveData<List<CountriesItem>> = _countriesList
+
+    init {
+        _masterCountriesList.observeForever { masterList ->
+            _countriesList.value = masterList
+        }
+    }
 
     fun handleIntent(intent: CountriesIntent) {
         when (intent) {
             is CountriesIntent.getCountries -> {
-                getCountries()
+                setCountriesMasterList()
             }
         }
     }
 
-    private fun getCountries() {
+    fun filterCountriesListByName(query: String) {
+        _countriesList.postValue(
+            _masterCountriesList.value?.filter { country ->
+                country.name.take(query.length).equals(query, ignoreCase = true)
+            }
+        )
+    }
+
+    fun setCountriesListDefault() {
+        _countriesList.value = masterCountriesList.value
+    }
+
+    private fun setCountriesMasterList() {
         viewModelScope.launch {
             try {
-                _countriesList.postValue(countriesRepository.getCountries())
+                _masterCountriesList.postValue(countriesRepository.getCountries())
             } catch (e: Exception) {
                 Log.d(TAG, "getCountries: $e")
             }
--- app/src/main/java/com/ryanbalseiro/countrieslist/ui/ListPage.kt
+++ app/src/main/java/com/ryanbalseiro/countrieslist/ui/ListPage.kt
@@ -1,43 +1,93 @@
 package com.ryanbalseiro.countrieslist.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ryanbalseiro.countrieslist.CountriesViewModel
+import com.ryanbalseiro.countrieslist.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListPage(viewModel: CountriesViewModel) {
-    val countriesList2 = viewModel.countriesList.observeAsState()
-    LazyColumn(
+    val countriesLocalList = viewModel.countriesList.observeAsState()
+    val queryState = remember { mutableStateOf("") }
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        item { Spacer(modifier = Modifier.height(20.dp)) }
-        if (!countriesList2.value.isNullOrEmpty()) {
-            for (country in countriesList2.value!!) {
-                item { CountryCard(
-                    capital = country.capital,
-                    code = country.code,
-                    name = country.name,
-                    region = country.region
-                ) }
-                item { Spacer(modifier = Modifier.height(6.dp)) }
+        SearchBar(
+            query = queryState.value,
+            onQueryChange = {
+                queryState.value = it
+                if (queryState.value.isEmpty() || queryState.value.isBlank()) {
+                    viewModel.setCountriesListDefault()
+                } else {
+                    viewModel.filterCountriesListByName(queryState.value)
+                }
+            },
+            onSearch = {
+                //add ability to filter by capital as well
+            },
+            active = false,
+            onActiveChange = {},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = true,
+            placeholder = { Text("Search by country name") },
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.search_icon),
+                    contentDescription = "Search Icon"
+                )
             }
-        } else {
-            item { Text("No countries found.") }
+        ) {
+            //content of the search bar, if needed
+        }
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item { Spacer(modifier = Modifier.height(20.dp)) }
+            if (!countriesLocalList.value.isNullOrEmpty()) {
+                for (country in countriesLocalList.value!!) {
+                    item {
+                        CountryCard(
+                            capital = country.capital,
+                            code = country.code,
+                            name = country.name,
+                            region = country.region
+                        )
+                    }
+                    item { Spacer(modifier = Modifier.height(6.dp)) }
+                }
+            } else {
+                item { Text("No countries found.") }
+            }
         }
     }
+
 }
 
 @Preview
